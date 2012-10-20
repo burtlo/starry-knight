3a51950d2fa620687f73882f066c3250fa52edb9
Franklin Webber <franklin.webber@gmail.com>
2012-10-19 21:46:52 -0400

    Added fading title

--- scenes/title_transition_scene.rb
+++ scenes/title_transition_scene.rb
@@ -18,9 +18,39 @@ class ScriptedPlayer
 
 end
 
+class Title
+
+  attr_accessor :x, :y, :z, :x_factor, :y_factor, :color, :alpha, :text
+
+  attr_accessor :window
+
+  def alpha
+    color.alpha
+  end
+
+  def font
+    @font ||= Gosu::Font.new(window, Gosu::default_font_name, 20)
+  end
+
+  def color=(value)
+    @color = Gosu::Color.new(value.to_i(16))
+  end
+
+  def alpha=(value)
+    color.alpha = value.floor
+  end
+
+  def draw
+    font.draw text, x, y, z, x_factor, y_factor, color
+  end
+
+end
+
 class TitleTransitionScene < Metro::Scene
 
   attr_reader :player
+
+  attr_reader :title
 
   attr_reader :animations
 
@@ -29,22 +59,48 @@ class TitleTransitionScene < Metro::Scene
   end
 
   def prepare_transition_from(title_scene)
-    logo = title_scene.view['logo']
+    hero_logo = title_scene.view['logo']
     @player = ScriptedPlayer.new
-    player.x = logo['x']
-    player.y = logo['y']
+    player.x = hero_logo['x']
+    player.y = hero_logo['y']
+
+    title_label = title_scene.view['title']
+    @title = Title.new
+    title.text = title_label['text']
+    title.x = title_label['x']
+    title.y = title_label['y']
+    title.z = title_label['z-order']
+    title.x_factor = title_label['x-factor']
+    title.y_factor = title_label['y-factor']
+    title.color = title_label['color']
   end
 
   def show
     player.window = window
+    title.window = window
+    add_player_animation
+    add_title_fade_animation
+  end
+
+  def add_player_animation
     final_x, final_y = Metro::Game.center
 
     animation = ImplicitAnimation.new actor: player,
       to: { x: final_x, y: final_y, angle: -360.0 },
       interval: 80.0,
       easing: :ease_in,
-      context: self,
-      completed: lambda { |scene| transition_to :main }
+      context: self
+
+    animation.on_complete do
+      transition_to :main
+    end
+
+    animations.push animation
+  end
+
+  def add_title_fade_animation
+    animation = ImplicitAnimation.new actor: title,
+      to: { alpha: 0 }, interval: 70.0
 
     animations.push animation
   end
@@ -61,6 +117,7 @@ class TitleTransitionScene < Metro::Scene
 
   def draw
     player.draw
+    title.draw
   end
 
 end
