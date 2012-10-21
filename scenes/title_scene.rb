5a508e157007770c112664b3bafd51e241a0875d
Franklin Webber <franklin.webber@gmail.com>
2012-10-21 13:18:44 -0400

    Game name is set in the metro file

--- scenes/title_scene.rb
+++ scenes/title_scene.rb
@@ -2,10 +2,6 @@ class TitleScene < Metro::Scene
 
   draws :title, :logo, :menu
 
-  def show
-    window.caption = "Starry Knight"
-  end
-
   def start_game
     transition_to :title_transition
   end
