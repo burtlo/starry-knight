bdece931f232649efcb0bb9522a28cff82b130ef
Franklin Webber <franklin.webber@gmail.com>
2012-10-21 20:28:03 -0400

    Animations now use the Scene#animate helper

--- scenes/pre_title_scene.rb
+++ scenes/pre_title_scene.rb
@@ -3,8 +3,15 @@ class PreTitleScene < Metro::Scene
   draws :title, :logo, :star, :star2, :star3
 
   def show
-    move_and_fade_in_logo
-    title_fade_in
+
+    animate actor: logo, to: { y: 80, alpha: 50 }, interval: 120 do
+      animate actor: logo, to: { alpha: 255 }, interval: 40 do
+        transition_to :title
+      end
+    end
+
+    animate actor: title, to: { alpha: 255 }, interval: 160
+
   end
 
   def events(e)
@@ -16,35 +23,5 @@ class PreTitleScene < Metro::Scene
   def update ; end
 
   def draw ; end
-
-  def move_and_fade_in_logo
-    final_x, final_y = Metro::Game.center
-
-    move_with_partial_fade = Metro::ImplicitAnimation.new actor: logo,
-      to: { y: 80, alpha: 50 },
-      interval: 120,
-      context: self
-
-    move_with_partial_fade.on_complete do
-
-      full_fade_in = Metro::ImplicitAnimation.new actor: logo,
-        to: { alpha: 255 },
-        interval: 40,
-        context: self
-
-      full_fade_in.on_complete do
-        transition_to :title
-      end
-
-      enqueue full_fade_in
-    end
-
-    enqueue move_with_partial_fade
-  end
-
-  def title_fade_in
-    enqueue Metro::ImplicitAnimation.new actor: title,
-      to: { alpha: 255 }, interval: 200
-  end
 
 end
--- scenes/title_transition_scene.rb
+++ scenes/title_transition_scene.rb
@@ -4,8 +4,13 @@ class TitleTransitionScene < Metro::Scene
   draw :title, model: 'metro::models::label', from: :previous_scene
 
   def show
-    add_player_animation
-    add_title_fade_animation
+    final_x, final_y = Metro::Game.center
+    
+    animate actor: logo, to: { x: final_x, y: final_y, angle: -360.0 }, interval: 80, easing: :ease_in do
+      transition_to :main
+    end
+
+    animate actor: title, to: { alpha: 0 }, interval: 70
   end
 
   def events(e)
@@ -17,28 +22,5 @@ class TitleTransitionScene < Metro::Scene
   def update ; end
 
   def draw ; end
-
-  def add_player_animation
-    final_x, final_y = Metro::Game.center
-
-    animation = Metro::ImplicitAnimation.new actor: logo,
-      to: { x: final_x, y: final_y, angle: -360.0 },
-      interval: 80,
-      easing: :ease_in,
-      context: self
-
-    animation.on_complete do
-      transition_to :main
-    end
-
-    enqueue animation
-  end
-
-  def add_title_fade_animation
-    animation = Metro::ImplicitAnimation.new actor: title,
-      to: { alpha: 0 }, interval: 70
-
-    enqueue animation
-  end
 
 end
