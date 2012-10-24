3987ccd7a4f8351dbbe4739dcdb8acf57324bfaa
Franklin Webber <franklin.webber@gmail.com>
2012-10-23 23:25:22 -0400

    Added fade to galaxy in title screen

--- scenes/title_scene.rb
+++ scenes/title_scene.rb
@@ -4,6 +4,10 @@ class TitleScene < Metro::Scene
 
   draw :menu, options: [ 'Start Game', 'Exit' ]
 
+  def show
+    animate actor: galaxy, to: { alpha: 100 }, interval: 60
+  end
+
   def start_game
     transition_to :title_transition
   end
