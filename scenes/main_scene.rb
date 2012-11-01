a23ad4e66e4933866c46e9a778d651d9a0dfd354
Franklin Webber <franklin.webber@gmail.com>
2012-10-31 18:26:40 -0600

    Implmentation with edit functionality

--- scenes/main_scene.rb
+++ scenes/main_scene.rb
@@ -8,6 +8,18 @@ class MainScene < Metro::Scene
     transition_to :title
   end
 
+  event :on_up, KbE do
+    # TODO: this should instead transition to an edit scene of the current scene.
+    # enable_edit_mode
+    log.debug "Going to edit mode"
+    transition_to self, with: :edit
+  end
+
+  event :on_up, KbR do
+    Metro.reload!
+    transition_to self.class.scene_name
+  end
+
   def show
     player.warp *Game.center
   end
--- scenes/pre_title_scene.rb
+++ scenes/pre_title_scene.rb
@@ -1,8 +1,8 @@
 class PreTitleScene < Metro::Scene
 
-  draws :title, :logo, :star, :star2, :star3
+  draws :title, :logo, :star, :star2, :star3, :galaxy
 
-  animate :logo, to: { y: 80, alpha: 50 }, interval: 2.seconds do
+  animate :logo, to: { y: 147, alpha: 50 }, interval: 2.seconds do
     animate :logo, to: { alpha: 255 }, interval: 1.second do
       transition_to :title
     end
