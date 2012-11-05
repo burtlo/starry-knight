19b1bb318cdb4751d4841a7209d88ac9abd95887
Franklin Webber <franklin.webber@gmail.com>
2012-11-05 13:21:52 -0500

    Set reload to be on Ctrl+r

--- scenes/game_scene.rb
+++ scenes/game_scene.rb
@@ -1,6 +1,8 @@
 class GameScene < Metro::Scene
-  event :on_up, KbR do
-    Metro.reload!
-    transition_to scene_name
+  event :on_up, KbR do |event|
+    if event.control?
+      Metro.reload!
+      transition_to self.class.scene_name
+    end
   end
 end
