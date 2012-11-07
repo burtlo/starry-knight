9e79d569698aa0815c25259d26efca29fe4a9ff6
Franklin Webber <franklin.webber@gmail.com>
2012-11-07 01:02:40 -0500

    Cleaned up Star

--- models/star.rb
+++ models/star.rb
@@ -1,8 +1,8 @@
 class Star < Metro::Model
 
-  property :animation, default: { path: "star.png", 
+  property :animation, default: { path: "star.png",
     dimensions: Metro::Dimensions.of(25,25) }
-  # property :image_path, type: :text, default: "missing.png"
+
   property :color
 
   attr_writer :x, :y, :animation
@@ -10,14 +10,10 @@ class Star < Metro::Model
   def after_initialize
     self.color = "rgba(#{rand_between(40,256)},#{rand_between(40,256)},#{rand_between(40,256)},1.0)"
   end
-  
+
   def rand_between(bottom,top)
     rand(top-bottom) + bottom
   end
-
-  # def animation
-  #   @animation ||= Gosu::Image::load_tiles(window, asset_path(image_path), 16, 16, false)
-  # end
 
   def x
     @x ||= rand * Game.width
