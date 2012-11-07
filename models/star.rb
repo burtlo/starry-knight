dce664ec5a38750874595cd1af09c76ea5d1e07c
Franklin Webber <franklin.webber@gmail.com>
2012-11-07 00:56:04 -0500

    Using the animation property

--- models/star.rb
+++ models/star.rb
@@ -1,21 +1,23 @@
 class Star < Metro::Model
 
-  property :image_path, type: :text, default: "star.png"
+  property :animation, default: { path: "star.png", 
+    dimensions: Metro::Dimensions.of(25,25) }
+  # property :image_path, type: :text, default: "missing.png"
   property :color
 
   attr_writer :x, :y, :animation
 
   def after_initialize
-    self.color = Gosu::Color.new "rgba(#{rand_between(40,256)},#{rand_between(40,256)},#{rand_between(40,256)},1.0)"
+    self.color = "rgba(#{rand_between(40,256)},#{rand_between(40,256)},#{rand_between(40,256)},1.0)"
   end
   
   def rand_between(bottom,top)
     rand(top-bottom) + bottom
   end
 
-  def animation
-    @animation ||= Gosu::Image::load_tiles(window, asset_path(image_path), 25, 25, false)
-  end
+  # def animation
+  #   @animation ||= Gosu::Image::load_tiles(window, asset_path(image_path), 16, 16, false)
+  # end
 
   def x
     @x ||= rand * Game.width
@@ -26,7 +28,7 @@ class Star < Metro::Model
   end
 
   def draw
-    img = animation[Gosu::milliseconds / 100 % animation.size ]
+    img = animation.image
     img.draw(x - img.width / 2.0, y - img.height / 2.0, 1, 1, 1, color, :add)
   end
 end
